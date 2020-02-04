4064aa520342f5850cf2ac946c41f50f2907613a
John Afolabi <johnafolabi.da@gmail.com>
2020-02-04 23:14:42 +0100

    Add production knex configuration

--- knexfile.js
+++ knexfile.js
@@ -38,5 +38,15 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       }
     }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 };
