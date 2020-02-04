670a3ead96ec7aa88adb2b1f92e446ba55b08a87
John Afolabi <johnafolabi.da@gmail.com>
2020-02-04 19:28:20 +0100

    Use environment variable, Add test knex config

--- knexfile.js
+++ knexfile.js
@@ -19,5 +19,24 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       }
     }
+  },
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/test.db3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
+    }
   }
 };
