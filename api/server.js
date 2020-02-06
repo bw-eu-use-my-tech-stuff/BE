2ba697a3f0938e74cc6b457317a8f271015f698c
John Afolabi <johnafolabi.da@gmail.com>
2020-02-06 11:48:53 +0100

    Write endpoints for renting equipments

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 const authRouter = require("../routes/authRouter");
 const equipmentRouter = require("../routes/equipmentRouter");
+const rentRouter = require("../routes/rentRouter");
 
 const server = express();
 
@@ -12,6 +13,7 @@ server.use(express.json());
 
 server.use("/api/auth", authRouter);
 server.use("/api/equipments", equipmentRouter);
+server.use("/api/rent", rentRouter);
 
 server.get("/api", (req, res) => {
   res.status(200).json({ message: `This server is working correctly` });
