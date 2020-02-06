d230deefaba15449c8660ca56bd448c146b8aa01
John Afolabi <johnafolabi.da@gmail.com>
2020-02-06 15:48:16 +0100

    Clean up route

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,14 +1,4 @@
-const {
-  addUser,
-  getUserById,
-  getUsers,
-  findUser
-} = require("../helpers/authModel");
-const {
-  restrictToPoster,
-  restrictToOwners,
-  restrictToRenters
-} = require("../middleware/restricted");
+const { addUser, findUser } = require("../helpers/authModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const express = require("express");
@@ -55,55 +45,6 @@ router.post("/login", (req, res) => {
       });
     });
 });
-
-// router.get("/users", restrictToOwners, (req, res, next) => {
-//   Users.getUsers()
-//     .then(users => {
-//       if (users) {
-//         res
-//           .status(200)
-//           .json(users.map(user => ({ id: user.id, username: user.username })));
-//       } else {
-//         next({ message: "No users were found", status: 404 });
-//       }
-//     })
-//     .catch(next);
-// });
-
-// router.get("/users/:id", restrictToPoster, validateUserId, (req, res) => {
-//   res.status(200).json(req.user);
-// });
-
-// router.put("/users/:id", restrictToOwners, validateUserId, validateUserBody, (req, res, next) => {
-//   Users.update(req.body, req.user.id)
-//     .then(updatedScheme => {
-//       res.status(200).json(updatedScheme);
-//     })
-//     .catch(next);
-// });
-
-// router.delete("/users/:id", restrictToOwners, validateUserId, (req, res, next) => {
-//   Users.remove(req.user.id)
-//     .then(() => {
-//       res.status(204).json(req.user);
-//     })
-//     .catch(next);
-// });
-
-// router.use((error, req, res, next) => {
-//   res
-//     .status(error.status || 500)
-//     .json({
-//       file: "user-router",
-//       //headers: req.headers,
-//       //protocol: req.protocol,
-//       method: req.method,
-//       url: req.url,
-//       status: error.status || 500,
-//       message: error.message
-//     })
-//     .end();
-// });
 
 function generateToken(user) {
   return jwt.sign(
